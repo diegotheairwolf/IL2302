adfacd35b5bb1692a7030bbe91f79ebdfcf37eb9
Diego Moreno <diego.ms90@gmail.com>
2020-02-22 18:26:44 +0100

    testing multiplexer
    added multiplexer and additional pins for the 9th capacitor

--- arduino/tao/tao.ino
+++ arduino/tao/tao.ino
@@ -13,17 +13,21 @@
  *
  *  Hardware setup:
  *  Test Capacitor between common point and ground (positive side of an electrolytic capacitor  to common)
- *  Test Resistor between chargePin and common point
- *  220 ohm resistor between dischargePin and common point
+ *  Test Resistor between chargePin13 and common point
+ *  220 ohm resistor between dischargePin11 and common point
  *  Wire between common point and analogPin (A/D input)
  */
 
 #define analogPin      0          // analog pin for measuring capacitor voltage
-#define chargePin      13         // pin to charge the capacitor - connected to one end of the charging resistor
-#define dischargePin   11         // pin to discharge the capacitor
-#define resistorValue  3300000.0F   // change this to whatever resistor value you are using
+#define chargePin13      13         // pin to charge the capacitor - connected to one end of the charging resistor
+#define dischargePin11   11         // pin to discharge the capacitor
+#define chargePin12      12         // pin to charge the capacitor - connected to one end of the charging resistor
+#define dischargePin10   10         // pin to discharge the capacitor
+#define A              4          // pin to select multiplexer channel. LSB
+#define B              3          // pin to select multiplexer channel.
+#define C              2          // pin to select multiplexer channel. MSB
+#define resistorValue  1000000.0F   // change this to whatever resistor value you are using
                                   // F formatter tells compliler it's a floating point value
-
 // defines for setting and clearing register bits
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
@@ -38,9 +42,18 @@ float microFarads;                // floating point variable to preserve precisi
 float nanoFarads;
 
 void setup(){
-  pinMode(chargePin, OUTPUT);     // set chargePin to output
-  digitalWrite(chargePin, LOW);  
+  pinMode(chargePin13, OUTPUT);     // set charegePin13 to output
+  digitalWrite(chargePin13, LOW);
+  pinMode(chargePin12, OUTPUT);     // set charegePin13 to output
+  digitalWrite(chargePin12, LOW);   
 
+  pinMode(A, OUTPUT);     // pin to select multiplexer channel. LSB
+  digitalWrite(A, LOW);
+  pinMode(B, OUTPUT);     // pin to select multiplexer channel.
+  digitalWrite(B, LOW);
+  pinMode(C, OUTPUT);     // pin to select multiplexer channel. MSB
+  digitalWrite(C, LOW);
+  
   Serial.begin(9600);             // initialize serial transmission for debugging
 
   // set prescale to 16
@@ -50,40 +63,45 @@ void setup(){
 }
 
 void loop(){
-  digitalWrite(chargePin, HIGH);  // set chargePin HIGH and capacitor charging
-  startTime = micros();
+  digitalWrite(chargePin13, HIGH);  // set charegePin13 HIGH and capacitor charging
+  digitalWrite(chargePin12, HIGH);  // set charegePin13 HIGH and capacitor charging
+  startTime = millis();
+  
+//  startTime = millis();
 
   while(analogRead(analogPin) < 648){       // 647 is 63.2% of 1023, which corresponds to full-scale voltage
   }
 
-  elapsedTime= micros() - startTime;
- // convert microseconds to seconds ( 10^-6 ) and Farads to picoFarads ( 10^9 ),  net 10^6 (1000000)  
-  microFarads = ((float)elapsedTime / resistorValue) * 1000000.00000;  
-  //Serial.print(elapsedTime);       // print the value to serial port
-  //Serial.print(" uS    ");         // print units and carriage return
+  elapsedTime= millis() - startTime;
+ // convert milliseconds to seconds ( 10^-3 ) and Farads to microFarads ( 10^6 ),  net 10^3 (1000)  
+  microFarads = ((float)elapsedTime / resistorValue) * 1000.0;  
+  Serial.print(elapsedTime);       // print the value to serial port
+  Serial.print(" mS    ");         // print units and carriage return
 
 
-  //if (microFarads > 1){
-    Serial.println((float)microFarads*1.0);       // print the value to serial port
-//    Serial.println(" picoFarads");         // print units and carriage return
-  //}
-  //else
-  //{
+//  if (microFarads > 1){
+    Serial.print((float)microFarads,5);       // print the value to serial port
+    Serial.println(" microFarads");         // print units and carriage return
+//  }
+//  else
+//  {
     // if value is smaller than one microFarad, convert to nanoFarads (10^-9 Farad).
     // This is  a workaround because Serial.print will not print floats
 
-  //  nanoFarads = microFarads * 1000.0;      // multiply by 1000 to convert to nanoFarads (10^-9 Farads)
-  //  Serial.print((float)nanoFarads);         // print the value to serial port
-  //  Serial.println(" nanoFarads");          // print units and carriage return
-  //}
+//    nanoFarads = microFarads * 1000.0;      // multiply by 1000 to convert to nanoFarads (10^-9 Farads)
+//    Serial.print((float)nanoFarads);         // print the value to serial port
+//    Serial.println(" nanoFarads");          // print units and carriage return
+//  }
 
   /* dicharge the capacitor  */
-  digitalWrite(chargePin, LOW);             // set charge pin to  LOW
-  pinMode(dischargePin, OUTPUT);            // set discharge pin to output
-  digitalWrite(dischargePin, LOW);          // set discharge pin LOW
-  delay(250);                               // delay in miliseconds
+  digitalWrite(chargePin13, LOW);             // set charge pin to  LOW
+  digitalWrite(chargePin12, LOW);             // set charge pin to  LOW
+  pinMode(dischargePin11, OUTPUT);            // set discharge pin to output
+  digitalWrite(dischargePin11, LOW);          // set discharge pin LOW
   while(analogRead(analogPin) > 0){         // wait until capacitor is completely discharged
   }
 
-  pinMode(dischargePin, INPUT);            // set discharge pin back to input
+  pinMode(dischargePin11, INPUT);            // set discharge pin back to input
+
+  delay(100);
 }
